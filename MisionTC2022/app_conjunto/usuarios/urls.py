0926bbed4c17ac0d2c9a03920fd4434316fcefae
Luis Marín <luismarin01@gmail.com>
2022-09-10 22:42:48 -0500

    Nueva ruta un solo resultado

--- MisionTC2022/app_conjunto/usuarios/urls.py
+++ MisionTC2022/app_conjunto/usuarios/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('add', views.newTipo_Usuario, name='addTipo_Usuario'),
     path('get', views.getTipo_Usuario, name='getTipo_Usuario'),
+    path('getOneTipo_Usuario/<int:codigo_usuario>', views.getOneTipo_Usuario, name='getOneTipo_Usuario'),
+      
+  
 ]
--- MisionTC2022/app_conjunto/usuarios/views.py
+++ MisionTC2022/app_conjunto/usuarios/views.py
@@ -19,7 +19,7 @@ def newTipo_Usuario (request):
                 tipo_usuario = data["tipo_usuario"]
             )
             users.save()
-            return HttpResponse("Vas a agregar un nuevo Tipo de Usuario")
+            return HttpResponse("Nuevo TIPO DE USUARIO agredado con Exito!")
         except:
             return HttpResponseBadRequest("Error en los datos envidos ABC")
     else:
@@ -28,11 +28,26 @@ def newTipo_Usuario (request):
 def getTipo_Usuario(request):
     if request.method == 'GET':
         users = Tipo_Usuario.objects.all()
+        if (not users):
+            return HttpResponseBadRequest("No existe ese Tipo de Usuario en la base de datos")
         allUserData = []
         for x in users:
             data = {"Tipo_usuario" : x.tipo_usuario, "Codigo_Usuario" : x.codigo_usuario}
             allUserData.append(data)
         dataJson = json.dumps(allUserData)
+        resp = HttpResponse()
+        resp.content = dataJson
+        return resp
+    else:
+        return HttpResponseNotAllowed(['GET'], 'METODO NO VALIDO') 
+        
+def getOneTipo_Usuario(request, codigo_usuario):
+    if request.method == 'GET':
+        user = Tipo_Usuario.objects.filter(codigo_usuario = codigo_usuario).first()
+        if (not user):
+            return HttpResponseBadRequest("No existe ese Tipo de Usuario con ese codigo")
+        data = {"Tipo_usuario" : user.tipo_usuario, "Codigo_Usuario" : user.codigo_usuario}
+        dataJson = json.dumps(data)
         resp = HttpResponse()
         resp.content = dataJson
         return resp
